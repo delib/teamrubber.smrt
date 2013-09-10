b1ab46061ab283a96171c94753177e3109eb9a29
Matthew Wilkes <git@matthewwilkes.name>
2013-09-10 13:18:07 +0100

    Remove IRC bot

--- smrt/__init__.py
+++ smrt/__init__.py
@@ -1,11 +1,9 @@
-from smrt.irc.bot import IRC
 from pyramid.config import Configurator
 from pyramid_zodbconn import get_connection
 from .models import appmaker
 
 import signal, sys
 
-bot = None
 
 def root_factory(request):
     conn = get_connection(request)
@@ -13,7 +11,6 @@ def root_factory(request):
 
 
 def main(global_config, **settings):
-    global bot
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(root_factory=root_factory, settings=settings)
@@ -24,26 +21,5 @@ def main(global_config, **settings):
     config.add_route("view_month", "/period/{year}/{month}")
     config.add_route("view_year", "/period/{year}")
     config.scan()
-
-    # Start IRC bot for controlling
-    try:
-        IRC.host = settings["irc_host"]
-        IRC.port = int(settings["irc_port"])
-        IRC.nick = settings["irc_bot_nick"]
-        IRC.ident = settings["irc_bot_nick"]
-        IRC.realname = settings["irc_bot_name"]
-        IRC.chan = settings["irc_channel"]
-        bot = IRC()
-    except Exception, e:
-        print "Failed to bring up IRC bot: %s" % e
-
     return config.make_wsgi_app()
 
-# Handle for clean shutdown
-def signal_handler(signal, frame):
-    print "Stopping, hang on..."
-    if bot != None:
-        bot.stop()
-    sys.exit()
-
-signal.signal(signal.SIGINT, signal_handler)
